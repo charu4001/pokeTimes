8c11b3f692716d77336cf8987d4d4a647bd1f995
Charu Pandey <charu.pandey@plainsmidstream.com>
2020-06-03 23:21:30 -0600

    Added text input for city

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,36 +8,40 @@ import Undraw_winter_magic_5xu2 from '../undraw_winter_magic_5xu2.png';
 import Undraw_windy_day_x63l from '../undraw_windy_day_x63l.png';
 import GolfR from '../golfR.jpg';
 
+/*
+Todos :
+FReeze Button on click 
+Modularize component - pass city name as prop 
+Refine the look and feel
+*/
+
 class Home extends Component {
-//const Home =() =>{
-    // state ={
-    //     posts: []
-    // }
-    // componentDidMount(){
-    //     console.log(new Date().toLocaleString());
-    //     axios.get('https://jsonplaceholder.typicode.com/posts/')
-    //     .then(res => {
-    //         console.log(res);
-    //         this.setState({
-    //             posts: res.data.slice(0,10)
-    //         });
-    //         console.log(new Date().toLocaleString());
+    state ={
+            wthr:[], 
+            city:''
+        };
 
-    //         console.log(this.state);
-    //     })
-    // }
+    handleChange = (e) => {
 
-    state ={wthr:[]};
+        console.log('inside handle click');
+
+        this.setState({[e.target.name]:e.target.value})
+        
+        console.log(e.target.name);
+    }
 
     handleclick = (e) => {
         
         e.preventDefault();
+        
+        console.log(e);
 
-        console.log('Button Clicked');
+        console.log('Button Clicked, City Below');
+        console.log(this.state.city);
 
         const apiKey = 'e1a93138d9f69da2b14fec4cd4b09e13';
         const url = 'https://api.openweathermap.org/data/2.5/weather?q=';//{city name}&appid={your api key}';
-        const cityName = 'Calgary';
+        const cityName = this.state.city;//'Calgary';
 
         const completeUrl = url + cityName +'&appid=' + apiKey;
 
@@ -59,12 +63,15 @@ class Home extends Component {
         console.log(this.state);
         console.log('Length below');
         console.log(wthr.id);
+
+        const myCity =(<input type="text" placeholder = 'Your city' name="city" value={this.state.city} onChange={this.handleChange}></input>);
+        console.log('myCity below');
+        console.log(myCity);
         
         const myButton = ( <button onClick={this.handleclick}>Get Temperature</button>);
 
         const weath = wthr.id ? (
-            //wthr.map(w => {
-                //return(
+            
                     <div>
                             <p>City: {wthr.name}</p>
                             <p>Temperature: {wthr.main.temp}</p>
@@ -76,8 +83,8 @@ class Home extends Component {
                             {console.log(wthr.weather[0].main)}
                             {(() => {
                                 switch (wthr.weather[0].main) {
-                                case "Clouds_": return (<img src = {Undraw_raining_sguh} alt='undraw_raining_sguh'/>);
-                                case "Sunny": return (<img src ={Undraw_sunny_day_bk3m} alt='undraw_sunny_day_bk3m'/>);
+                                case "Clouds": return (<img src = {Undraw_raining_sguh} alt='undraw_raining_sguh'/>);
+                                case "Clear": return (<img src ={Undraw_sunny_day_bk3m} alt='undraw_sunny_day_bk3m'/>);
                                 case "Windy":  return (<img src ={Undraw_windy_day_x63l} alt='undraw_windy_day_x63l'/>);
                                 case "Snow":  return (<img src ={Undraw_winter_magic_5xu2} alt='undraw_winter_magic_5xu2'/>);
                                 default:      return (<img src ={GolfR} alt='golfR'/>);
@@ -85,8 +92,7 @@ class Home extends Component {
                             })()}
                             </p>
                     </div>
-                //)
-            //})
+             
 
         ) : (
 
@@ -95,28 +101,11 @@ class Home extends Component {
         )
 
 
-        // const postList = posts.length ? (
-        //     posts.map(post =>{
-        //         return(
-        //             <div className="post card" key={post.id}>
-        //             <img src = {Pic} alt="pic"/>
-        //             <div className="card-content">
-        //                 <Link to ={'/' + post.id}>
-        //                 <span className="card-title red-text">{post.title}</span>
-        //                 </Link>
-        //                 <p>{post.body}</p>
-        //             </div>
-                    
-        //             </div>
-        //         )
-        //     })
-        // ) : (
-        //     <div className="center">No posts Yet </div>
-        // )
     return(
         <div className="container home">
             <h4 className="center">Home</h4>
-            <view>{myButton}</view>
+            <p>{myCity}</p>
+            <p>{myButton}</p>
             <view>{weath}</view>
         </div>
     )
